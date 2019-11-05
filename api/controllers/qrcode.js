5c7ccb911169779a1989b36fbd91cbb0fc5dcb72
beemargarida <up201505505@gcloud.fe.up.pt>
2019-11-05 20:38:57 +0000

    QRCode generation working

---
+++ api/controllers/qrcode.js
@@ -0,0 +1,49 @@
+const QRCode = require('qrcode');
+const uuid = require('uuid/v4');
+var ByteBuffer = require("bytebuffer");
+const nodeRSA = require('node-rsa');
+const fs = require('fs');
+const path = require('path');
+const iconv = require("iconv-lite");
+
+exports.peachQRCode = async function (req, res, next) {
+    generateQRCode("Peach", 0, 25, res, next);
+}
+
+function generateQRCode(name, priceEuros, priceCents, res, next) {
+    const prod_uuid = new Array();
+    uuid(null, prod_uuid, 0);
+    const prod_uuid_string = uuid({ random: prod_uuid });
+    const len = 4 + 16 + 4 + 4 + 1 + name.length;
+    if (name.length > 35) {
+        name = name.substring(0, 35);
+    }
+
+    const tag = Buffer.alloc(len);
+    tag.writeInt32BE(0x41636D65);
+    const uuid_bytes_most = Buffer.from(prod_uuid).slice(0, 8).readBigUInt64LE();
+    const uuid_bytes_less = Buffer.from(prod_uuid).slice(8, 16).readBigUInt64LE();
+    tag.writeBigUInt64LE(uuid_bytes_most, 4);
+    tag.writeBigUInt64LE(uuid_bytes_less, 12);
+    tag.writeInt32BE(priceEuros, 20);
+    tag.writeInt32BE(priceCents, 24);
+    tag[28] = name.length;
+    iconv.encode(name, 'ISO-8859-1').copy(tag, 29, 0, 5);
+
+    const key = new nodeRSA();
+    key.importKey(process.env.SUPERMARKET_PRIVATE_KEY, 'pkcs1-private')
+    key.setOptions({ encryptionScheme: 'pkcs1' });
+    const encrypted = key.encryptPrivate(tag, 'hex');
+
+    QRCode.toDataURL(encrypted)
+        .then(url => {
+            var base64Data = url.replace(/^data:image\/png;base64,/, "");
+            fs.writeFile(`QRCodes/${name}.png`, base64Data, 'base64', function (err) {
+                if (err) throw err
+                return res.status(200).sendFile(path.join(__dirname, '../QRCodes', `${name}.png`));
+            });
+        })
+        .catch(err => {
+            return next(err);
+        })
+}
--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -5,15 +6,20 @@ const nodeRSA = require('node-rsa');
 const errorHandler = require('./controllers/error');
 const authRoutes = require('./routes/auth');
 const supermarketRoutes = require('./routes/supermarket');
+const qrCodeRoutes = require('./routes/qrcode');
 const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
 
 const PORT = 8080;
 const app = express();
 
 // TODO: Get supermarket public key
-const key = new nodeRSA({b: 512});
-process.env.SUPERMARKET_PUBLIC_KEY = key.exportKey('public');
-process.env.SUPERMARKET_PRIVATE_KEY = key.exportKey('private');
+if(!process.env.SUPERMARKET_PUBLIC_KEY || !process.env.SUPERMARKET_PRIVATE_KEY){
+  const key = new nodeRSA({b: 512});
+  process.env.SUPERMARKET_PUBLIC_KEY = key.exportKey('public');
+  process.env.SUPERMARKET_PRIVATE_KEY = key.exportKey('private');
+}
+console.log(process.env.SUPERMARKET_PUBLIC_KEY);
+console.log(process.env.SUPERMARKET_PRIVATE_KEY);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,6 +29,8 @@ app.use('/supermarket', loginRequired, ensureCorrectUser, supermarketRoutes);
 
 //TODO: Add User routes
 //TODO: Add Payment Card routes ? -> check if needed
+
+app.use('/qrcode', qrCodeRoutes);
 
 app.use('/', (req, res, next) => {
   res.send('Welcome to ACME Electronic Supermarket!');
