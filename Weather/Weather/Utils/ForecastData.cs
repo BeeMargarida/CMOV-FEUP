2f4d32a82351ceefb42b2f6976271f2b8f043686
beemargarida <up201505505@fe.up.pt>
2019-12-01 14:40:00 +0000

    started city page

---
+++ Weather/Weather/Utils/ForecastData.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+
+namespace Weather.Utils
+{
+    class ForecastData
+    {
+        [JsonProperty("list")]
+        public ForecastEntry[] Entries { get; set; }
+    }
+
+    class ForecastEntry
+    {
+        [JsonProperty("dt")]
+        public string DateTime { get; set; }
+
+        [JsonProperty("weather")]
+        public WeatherStatus[] Weather { get; set; }
+
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+    }
+}
