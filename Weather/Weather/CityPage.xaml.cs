7f9b997f2d07a33519257bffe205ec9e616fc3cb
BeeMargarida <up201505505@gcloud.fe.up.pt>
2019-12-02 19:34:49 +0000

    minor setup

--- Weather/Weather/CityPage.xaml.cs
+++ Weather/Weather/CityPage.xaml.cs
@@ -1,14 +1,44 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Weather.Models;
 
 namespace Weather
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CityPage : TabbedPage
     {
-        public CityPage()
+        private City city;
+        public CityPage(City city)
         {
             InitializeComponent();
+            this.city = city;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //listView.ItemsSource = new List<City>();
+
+            if (IsBusy)
+                return;
+            try
+            {
+               
+            }
+            catch (Exception e)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "An error occurred",
+                    e.Message,
+                    "Ok"
+                );
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
--- Weather/Weather/TodayPage.xaml.cs
+++ Weather/Weather/TodayPage.xaml.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Weather.Models;
 
 namespace Weather
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TodayPage : ContentPage
     {
-        public TodayPage()
+
+        private City city;
+        public TodayPage(City city)
         {
             InitializeComponent();
+            this.city = city;
         }
     }
 }
